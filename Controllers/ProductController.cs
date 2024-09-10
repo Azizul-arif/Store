3052d8d679e5965f1202f5025381682b37cca0f6
Azizul Arif <azizularif94@gmail.com>
2024-09-10 21:24:57 +0600

    Product table frontend backend

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Ecommerce.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.Controllers
@@ -17,9 +18,75 @@ namespace Ecommerce.Controllers
             return View(productList.ToList());
         }
 
-        public IActionResult Create() 
+        public IActionResult Create()
         {
+            ViewBag.CategoryList = new SelectList(_db.Categories, "Id", "CategoryName");
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(product);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View("Error", "Index");
+        }
+
+        public IActionResult Update(int? productID)
+        {
+            ViewBag.CategoryList = new SelectList(_db.Categories, "Id", "CategoryName");
+            var productData = _db.Products.FirstOrDefault(u => u.Id == productID);
+            if (productData is not null)
+            {
+                return View(productData);
+            }
+            return View("Error","Home");
+        }
+        [HttpPost]
+        public IActionResult Update(Product productData)
+        {
+            ViewBag.CategoryList = new SelectList(_db.Categories, "Id", "CategoryName");
+            var productObj= _db.Products.FirstOrDefault(u=>u.Id == productData.Id);
+            if (productObj is not null)
+            {
+                productObj.Name = productData.Name;
+                productObj.Description = productData.Description;
+                productObj.CategoryId = productData.CategoryId;
+                productObj.Price = productData.Price;
+
+                _db.Products.Update(productObj);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View("Error","Home");
+
+        }
+        public IActionResult Delete(int? productID)
+        {
+            ViewBag.CategoryList = new SelectList(_db.Categories, "Id", "CategoryName");
+            var productData = _db.Products.FirstOrDefault(u => u.Id == productID);
+            if (productData is not null)
+            {
+                return View(productData);
+            }
+            return View("Error", "Home");
+        }
+        [HttpPost]
+        public IActionResult Delete(Product? productData)
+        {
+            ViewBag.CategoryList = new SelectList(_db.Categories, "Id", "CategoryName");
+            var productObj = _db.Products.FirstOrDefault(u => u.Id == productData.Id);
+            if (productData is not null)
+            {
+                _db.Products.Remove(productObj);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+                
+            }
+            return View("Error", "Home");
         }
     }
 }
--- obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+833b5b95c0c56d9f983ee41e6e75cdad1f468a28")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
