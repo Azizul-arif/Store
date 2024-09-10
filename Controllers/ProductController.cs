833b5b95c0c56d9f983ee41e6e75cdad1f468a28
Azizul Arif <azizularif94@gmail.com>
2024-09-10 06:48:52 +0600

    Color, Category,Product with seed data

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var productList = _db.Products.Include(u => u.Category);
+            return View(productList.ToList());
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+    }
+}
