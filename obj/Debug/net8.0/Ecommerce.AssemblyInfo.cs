98752b458f1e9ab0a4af48a410bcb604eb6f59c3
Azizul Arif <azizularif94@gmail.com>
2024-09-12 04:39:56 +0600

    changing branch

--- obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3052d8d679e5965f1202f5025381682b37cca0f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a27ccafdff4d6de72c3db0019259627e4008bbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
